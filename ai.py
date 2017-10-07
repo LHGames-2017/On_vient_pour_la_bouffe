3f8f55b1313a3860d82a1be882369cb54ceffbeb
MagicHack <emile.larose@gmail.com>
2017-10-07 19:34:31 -0400

    plzzzzz

--- ai.py
+++ ai.py
@@ -1,7 +1,9 @@
+#from __future__ import #print_function
 from flask import Flask, request
 from structs import *
 import json
 import numpy
+
 
 app = Flask(__name__)
 
@@ -59,7 +61,7 @@ def bot():
     encoded_map = map_json.encode()
     map_json = json.loads(encoded_map)
     p = map_json["Player"]
-    print "player:{}".format(p)
+    #print ("player:{}".format(p))
     pos = p["Position"]
     x = pos["X"]
     y = pos["Y"]
@@ -83,8 +85,139 @@ def bot():
 
         otherPlayers.append(player_info)
 
-    # return decision
-    return create_move_action(Point(x,y+1))
+    # jour a 10,10
+
+    #print(deserialized_map[10][10].Content)
+    #print(p["CarriedResources"])
+    closestMin = closestMinerals(deserialized_map, x, y)
+    closestH = closestHome(deserialized_map, x, y)
+
+    showMap(deserialized_map)
+    #print("joueur", pos)
+    #print("Maison", closestH[0], closestH[1])
+    #print("mine", closestMin[0], closestMin[1])
+
+
+    if isFull(p["CarriedResources"] , p["CarryingCapacity"]) :
+        #print("home")
+        coord = simpleGo(x, y, closestH[0], closestH[1])
+        return create_move_action(Point(coord[0], coord[1]))
+    elif (abs(x-closestMin[0]) + abs(y-closestMin[1]) == 1):
+        #print("mining")
+        return create_collect_action(Point(closestMin[0], closestMin[1]))
+    else :
+        #print("going mine")
+        coord = simpleGo(x, y, closestMin[0], closestMin[1])
+        return create_move_action(Point(coord[0], coord[1]))
+
+
+    #return decision
+
+tuiles = (".", "%", "M", "L", "R", "S", "P")
+
+def isFull(carried, total) : # isFull(p["CarriedResources"] , p["CarryingCapacity"])
+    if carried == total:
+        return True
+    else :
+        return False
+
+
+
+def simpleGo(xP,yP, x, y) :
+    if abs(xP-x) > abs(yP-y) :
+         if xP > x :
+             return (xP-1, yP)
+         else :
+            return (xP + 1, yP)
+    else :
+        if yP > y :
+           return (xP, yP - 1)
+
+        else :
+           return (xP, yP + 1)
+
+
+def closestHome(map, x, y) :
+    p = 10
+    minerais = [(0,0)]
+    minerais.pop(0)
+    for i in range (0,20):
+        for j in range (0,20) :
+            if tuiles[map[i][j].Content] == "M" :
+                minerais.append((i,j))
+
+    minX = -1
+    minY = -1
+    minDistance = 1000
+    for minerai in minerais :
+        distance = math.sqrt((minerai[0] - 10) * (minerai[0] - 10) + (minerai[1] -  10) * (minerai[1] - 10))
+        if distance < minDistance :
+            minDistance = distance
+            minX = minerai[0]
+            minY = minerai[1]
+
+    if minX == -1 or minY == -1:
+        return (minX, minY)
+
+    elif minX < 10 :
+        minX = x + minX - p
+    elif minX > 10 :
+        minX = x + minX + p
+    elif minX == 10:
+        minX = x
+
+
+    if minY < 10:
+        minY = y + minY - p
+    elif minY > 10:
+        minY = y + minY + p
+    elif minY == 10:
+        minY = y
+    return (minX, minY)
+
+def closestMinerals(map, x, y) :
+    p = 10
+    minerais = [(0,0)]
+    minerais.pop(0)
+    for i in range (0,20):
+        for j in range (0,20) :
+            if tuiles[map[i][j].Content] == "R" :
+                minerais.append((i,j))
+
+    minX = -1
+    minY = -1
+    minDistance = 1000
+    for minerai in minerais :
+        distance = math.sqrt((minerai[0] - 10) * (minerai[0] - 10) + (minerai[1] -  10) * (minerai[1] - 10))
+        if distance < minDistance :
+            minDistance = distance
+            minX = minerai[0]
+            minY = minerai[1]
+
+    if minX == -1 or minY == -1:
+        return (minX, minY)
+    elif minX > 10 :
+        minX = x + minX - p
+    elif minX > 10 :
+        minX = x + minX + p
+    elif minX == 10:
+        minX = x
+
+
+    if minY > 10:
+        minY = y + minY - p
+    elif minY < 10:
+        minY = y + minY + p
+    elif minY == 10:
+        minY = y
+    return (minX, minY)
+
+def showMap(map) :
+    for x in range(0,20) :
+        for y in range (0,20) :
+            y
+            #print (tuiles[int(map[x][y].Content)], end='', )
+        #print("\n", end='')
 
 @app.route("/", methods=["POST"])
 def reponse():
